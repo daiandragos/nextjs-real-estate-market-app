30fbd73b3b392a7dc284a6abc80fb606dd86a60b
Daian <daiandragos@yahoo.com>
2026-03-07 08:47:30 +0200

    feat: initial commit

---
+++ proxy.ts
@@ -0,0 +1,37 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/saved(.*)",
+  "/profile(.*)",
+]);
+
+const isOnboardingRoute = createRouteMatcher(["/onboarding(.*)"]);
+
+const isAgentRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/properties(.*)",
+  "/pricing(.*)",
+  "/sign-in(.*)",
+  "sign-up(.*)",
+  "/studio(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  // allow public routes
+  if (isPublicRoute(req)) {
+    return NextResponse.next();
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
