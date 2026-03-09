8a62510070b18d4ba743367ca019c24b9adba1f5
Daian <daiandragos@yahoo.com>
2026-03-09 09:24:16 +0200

    fix: temp fix for properties

--- components/property/property-grid.tsx
+++ components/property/property-grid.tsx
@@ -29,7 +29,7 @@ export function PropertyGrid({
       {properties.map((property) => (
         <PropertyCard
           key={property._id}
-          property={property}
+          property={property as any}
           onSave={onSave}
           isSaved={savedIds.includes(property._id)}
           showRemoveButton={showRemoveButton}
